7bc20e0d6773864cadf6958af1671197bcf5f885
rico <ricoantonio33@hotmail.com>
2019-12-31 15:18:55 +0700

    test

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import {Route,BrowserRouter} from 'react-router-dom'
+
+import Home from '../Home';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+        <Route path="/" exact component={Home}/>
+    </BrowserRouter>
   );
 }
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,7 +4,6 @@ export class Home extends Component {
     render() {
         return (
             <div>
-                
             </div>
         )
     }
