5003c8f0f317525141e3973bf407b54f4ac59366
rico <ricoantonio33@hotmail.com>
2020-01-01 16:39:09 +0700

    paralax

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,7 @@ function App() {
   return (
     < >
       <Header/>
-      <BrowserRouter>
-          <Route path="/" exact component={Home}/>
-      </BrowserRouter>
+      <Home/>
     </>
   );
 }
