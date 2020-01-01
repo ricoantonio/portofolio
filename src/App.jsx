576fd6e6738425b6e6812c22c94a1858712fbab7
rico <ricoantonio33@hotmail.com>
2020-01-01 21:04:00 +0700

    mouse detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
 import {Route,BrowserRouter} from 'react-router-dom'
 import './style/global.scss';
 import Home from './components/Home';
 import Header from './components/Header';
 
+export class App extends Component {
+  state={
+    mouseX:0,
+    mouseY:0
+  }
 
-
-function App() {
-  return (
-    < >
-      <Header/>
-      <Home/>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <div onMouseMove={(e)=>{this.setState({
+          mouseX:e.screenX,
+          mouseY:e.screenY
+        })}}>
+          <Header/>
+          <Home/>
+          <h1>{this.state.mouseX}</h1>
+          <h1>{this.state.mouseY}</h1>
+          
+          
+          
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
