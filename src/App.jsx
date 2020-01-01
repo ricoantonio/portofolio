a4a5fcb8d065fd7b78b05a9bf489043da29b5420
rico <ricoantonio33@hotmail.com>
2020-01-01 13:13:59 +0700

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import {Route,BrowserRouter} from 'react-router-dom'
-import '../../style/global.scss';
+import './style/global.scss';
+import Home from './components/Home';
+import Header from './components/Header';
 
-import Home from '../Home';
-import Header from '../Header';
 
 
 function App() {
